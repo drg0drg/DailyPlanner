de135940f10ef0080c6a2d5fe29edab92e43fa5f
drg0drg <dragomir_costin_dragos@yahoo.com>
2020-04-18 01:24:03 +0100

    Local storage stores user input for each hour, like an object. Still doesn't retain inputs after page refresh

--- script.js
+++ script.js
@@ -17,9 +17,37 @@
 
 
 
-var dayPlanForm = document.getElementById("day-plan-form")
-var plannerContainerEl = document.getElementById("planner-container")
 
+
+
+// ----------------- Variables------------------
+//----------------------------------------------
+var dayPlanForm = document.getElementById("day-plan-form");
+var plannerContainerEl = document.getElementById("planner-container");
+
+//The following is needed for the coloring loop, depending on hour
+//<currentHour> takes the hour only from the moment library. It outputs a string
+currentHour = moment().format("kk");
+console.log("current time (hour only) is  " + currentHour);
+inputArray = [];
+
+
+
+let storedInput = JSON.parse(localStorage.getItem("storedInput"));
+
+
+if (storedInput !== null) {
+    inputArray = storedInput;
+  } else {
+    inputArray = new Array(9);
+  }
+
+
+
+
+
+//----------------MomentJS----------------------
+//----------------------------------------------
 
 //Showing the date and time using Moment.js
 var nowMoment = moment();
@@ -32,22 +60,21 @@ nowMomentShown.innerHTML = nowMoment.format('DD-MMM-YYYY kk:mm');
 
 
 
-//The following is needed for the coloring loop, depending on hour
-//<currentHour> takes the hour only from the moment library. It outputs a string
-currentHour = moment().format("kk");
-console.log("current time (hour only) is  " + currentHour);
 
 
 
 
 
 
-// Dynamically building the rows in a for loop with JS
+// Dynamically building the rows in a <for loop> with JS
 for (let h=16; h<=24; h++){
+
+    let inputArrayIndex = h-16; 
 
     //Creating the row container
     const divRow = document.createElement("div");
     divRow.setAttribute("class", "div-row-dsk div-row-tab div-row-mob");
+    divRow.setAttribute("h-Index", h);
     plannerContainerEl.appendChild(divRow);
 
 
@@ -79,18 +106,27 @@ for (let h=16; h<=24; h++){
 
 
 
+    rowColorCode (divInput, h);
+
+
 
     //color coding function of hour
-    if (currentHour > h) {
-        divInput.style.backgroundColor = "rgb(240, 150, 150, 0.700)";
-        // console.log(typeof(currentHour));
-        // console.log(typeof(h));
-    } else if (currentHour < h){
-        divInput.style.backgroundColor = "rgb(158, 245, 158, 0.700)";
-    }else {
-        divInput.style.backgroundColor = "rgb(173, 173, 173, 0.700)";
-    }
+    // if (currentHour > h) {
+    //     divInput.style.backgroundColor = "rgb(240, 150, 150, 0.700)";
+    //     // console.log(typeof(currentHour));
+    //     // console.log(typeof(h));
+    // } else if (currentHour < h){
+    //     divInput.style.backgroundColor = "rgb(158, 245, 158, 0.700)";
+    // } else {
+    //     divInput.style.backgroundColor = "rgb(173, 173, 173, 0.700)";
+    // }
 
+
+    // var hour = inputArray[h];
+    // console.log(inputArray[18]);
+    // var task = localStorage.getItem("item" + hour);
+    // inputArray[h] = task.value;
+    // console.log(inputArray[18]);
 
 
 
@@ -107,10 +143,14 @@ for (let h=16; h<=24; h++){
     btnSave.addEventListener("click", function(e){
         e.preventDefault();
         var userInput = divInput.value;
-        localStorage.setItem("items", (userInput));
+        localStorage.setItem(h, (userInput));
+        inputArray[inputArrayIndex]=localStorage.setItem(h, (userInput));
     })
 
     // localStorage.getItem(("items"));
+
+
+
 
 
 
@@ -121,6 +161,26 @@ for (let h=16; h<=24; h++){
 
 
 }//end of for loop
+
+
+
+function rowColorCode (divInput, h) {
+    if (currentHour > h) {
+        divInput.style.backgroundColor = "rgb(240, 150, 150, 0.700)";
+        // console.log(typeof(currentHour));
+        // console.log(typeof(h));
+    } else if (currentHour < h){
+        divInput.style.backgroundColor = "rgb(158, 245, 158, 0.700)";
+    } else {
+        divInput.style.backgroundColor = "rgb(173, 173, 173, 0.700)";
+    }
+
+}
+
+
+
+
+
 
 
 
